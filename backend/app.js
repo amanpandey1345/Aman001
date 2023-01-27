40ccdb98eab0814daa5ddfd43e01c5dc335a699e
amanpandey1345 <70140546+amanpandey1345@users.noreply.github.com>
2023-01-27 12:08:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const errorMiddleware = require("./middleware/error")
 const app = express();
 const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser")
+const path = require("path");
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
